20bd8f30607e5b3b29b87c55c1b9f3a99b7e445b
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2023-08-31 13:04:46 +0900

    Update kubeadm-init.sh

--- gasida-k8s/kubeadm-init.sh
+++ gasida-k8s/kubeadm-init.sh
@@ -53,7 +53,7 @@ swapoff -a
 
 echo "[TASK 11] Install Kubernetes components (kubeadm, kubelet and kubectl) - v$2"
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v$2/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$2/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$2/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update >/dev/null 2>&1
 apt-get install -y kubelet=$2-00 kubectl=$2-00 kubeadm=$2-00 >/dev/null 2>&1
 apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
